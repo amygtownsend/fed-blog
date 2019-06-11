07b6875d5e7ac3f742209bd4ef1f2a3bcc4918a7
Amy Townsend <amygtownsend@gmail.com>
2019-06-11 10:25:27 -0400

    map data instead of for loop and rewrite state initialization

--- src/components/BlogExcerpts/index.js
+++ src/components/BlogExcerpts/index.js
@@ -4,47 +4,27 @@ import blogPostData from "../../data/blog-posts.js";
 import './styles.css';
 
 class BlogExcerpts extends Component {
-  constructor(props) {
-    super(props);
-    this.fullData = blogPostData;
-    this.state = {
-      data: this.fullData.slice(),
-    }
+  fullData = blogPostData;
+  state = {
+    data: [],
   }
 
-  createExcerpt = ({ key, picture, author, title, body, date }) => {
+  componentDidMount() {
+    this.setState({ 
+      data: this.fullData.slice()
+     })
+  }
+
+  renderExcerpt = ({ index, picture, author, title, body, date }) => {
     return (
-      <BlogExcerpt key={key} picture={picture} author={author} title={title} body={body} date={date}/>
+      <BlogExcerpt key={index} picture={picture} author={author} title={title} body={body} date={date}/>
     )
-  }
-
-  createExcerpts = (data) => {
-    const post = {
-      key: "",
-      picture: "",
-      author: "",
-      title: "",
-      body: "",
-      date: ""
-    }
-    let { key, picture, author, title, body, date } = post;
-    let excerpts = [];
-    data.forEach((r) => {
-      key = r.index;
-      picture = r.picture;
-      author = r.author;
-      title = r.title;
-      body = r.body;
-      date = r.date;
-      excerpts.push(this.createExcerpt({ key, picture, author, title, body, date }));
-    })
-    return excerpts;
   }
 
   render() {
     return (
       <div>
-        {this.createExcerpts(this.state.data)}
+        {this.state.data.map(this.renderExcerpt)}
       </div>
     )
   }
