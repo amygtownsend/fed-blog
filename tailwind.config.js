a9bff432c66493997c89b75768141276b302852e
Amy Townsend <amygtownsend@gmail.com>
2019-06-12 17:43:18 -0400

    add custom tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,339 @@
 module.exports = {
+  prefix: '',
+  important: false,
+  separator: ':',
   theme: {
-    extend: {}
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px'
+    },
+    colors: {
+      transparent: 'transparent',
+
+      black: '#000',
+      white: '#fff',
+
+      purple: '#A15693',
+      brown: '#433643',
+
+      gray: {
+        100: '#E0E0E0',
+        200: '#4F4F4F',
+        300: '#333'
+      },
+      blue: {
+        100: '#D7F0F3',
+        200: '#38A7B7'
+      },
+      green: {
+        100: '#F7F4E4',
+        200: '#D7BC27'
+      }
+    },
+    spacing: {
+      '0': '0',
+      '4': '0.25rem',
+      '8': '0.5rem',
+      '12': '0.75rem',
+      '14': '0.875rem',
+      '16': '1rem',
+      '22': '1.375rem',
+      '30': '1.875rem',
+      '184': '11.5rem',
+      '224': '14rem',
+      '244': '15.25rem'
+    },
+    backgroundColor: theme => theme('colors'),
+    backgroundPosition: {
+      bottom: 'bottom',
+      center: 'center',
+      left: 'left',
+      'left-bottom': 'left bottom',
+      'left-top': 'left top',
+      right: 'right',
+      'right-bottom': 'right bottom',
+      'right-top': 'right top',
+      top: 'top'
+    },
+    backgroundSize: {
+      auto: 'auto',
+      cover: 'cover',
+      contain: 'contain'
+    },
+    borderColor: theme => ({
+      ...theme('colors'),
+      default: theme('colors.gray.100', 'currentColor')
+    }),
+    borderRadius: {
+      none: '0',
+      default: '0.25rem',
+      '8': '0.5rem',
+      full: '9999px'
+    },
+    borderWidth: {
+      default: '1px',
+      '0': '0',
+      '2': '2px',
+      '4': '4px',
+      '8': '8px'
+    },
+    boxShadow: {
+      default: '0 2px 2px 0 rgba(0, 0, 0, 0.2)',
+      none: 'none'
+    },
+    container: {},
+    cursor: {
+      auto: 'auto',
+      default: 'default',
+      pointer: 'pointer',
+      wait: 'wait',
+      text: 'text',
+      move: 'move',
+      'not-allowed': 'not-allowed'
+    },
+    fill: {
+      current: 'currentColor'
+    },
+    flex: {
+      '1': '1 1 0%',
+      auto: '1 1 auto',
+      initial: '0 1 auto',
+      none: 'none'
+    },
+    flexGrow: {
+      '0': '0',
+      default: '1'
+    },
+    flexShrink: {
+      '0': '0',
+      default: '1'
+    },
+    fontFamily: {
+      sans: [
+        'Montserrat',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"'
+      ],
+      serif: [
+        'Merriweather',
+        'Georgia',
+        'Cambria',
+        '"Times New Roman"',
+        'Times',
+        'serif'
+      ],
+      mono: [
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        '"Liberation Mono"',
+        '"Courier New"',
+        'monospace'
+      ]
+    },
+    fontSize: {
+      '14': '0.875rem',
+      '15': '0.9375rem',
+      '16': '1rem',
+      '18': '1.125rem',
+      '22': '1.375rem',
+      '24': '1.5rem',
+      '36': '2.25rem',
+      '40': '2.5rem',
+      '48': '3rem',
+      '50': '3.125rem',
+      '64': '4rem',
+      '80': '5rem',
+      '100': '6.25rem'
+    },
+    fontWeight: {
+      extralight: '100',
+      thin: '200',
+      light: '300',
+      regular: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
+      extrabold: '800',
+      black: '900'
+    },
+    height: theme => ({
+      auto: 'auto',
+      ...theme('spacing'),
+      full: '100%',
+      screen: '100vh'
+    }),
+    inset: {
+      '0': '0',
+      auto: 'auto'
+    },
+    letterSpacing: {
+      tighter: '-0.05em',
+      tight: '-0.025em',
+      normal: '0',
+      wide: '0.025em',
+      wider: '0.05em',
+      widest: '0.1em'
+    },
+    lineHeight: {
+      none: '1',
+      tight: '1.25',
+      normal: '1.5',
+      relaxed: '1.625',
+      loose: '2'
+    },
+    listStyleType: {
+      none: 'none',
+      disc: 'disc',
+      decimal: 'decimal'
+    },
+    margin: (theme, { negative }) => ({
+      auto: 'auto',
+      ...theme('spacing'),
+      ...negative(theme('spacing'))
+    }),
+    maxHeight: {
+      full: '100%',
+      screen: '100vh'
+    },
+    maxWidth: {
+      '950': '59.375rem',
+      '994': '62.125rem',
+      full: '100%'
+    },
+    minHeight: {
+      '0': '0',
+      full: '100%',
+      screen: '100vh'
+    },
+    minWidth: {
+      '0': '0',
+      full: '100%'
+    },
+    objectPosition: {
+      bottom: 'bottom',
+      center: 'center',
+      left: 'left',
+      'left-bottom': 'left bottom',
+      'left-top': 'left top',
+      right: 'right',
+      'right-bottom': 'right bottom',
+      'right-top': 'right top',
+      top: 'top'
+    },
+    opacity: {
+      '0': '0',
+      '25': '0.25',
+      '50': '0.5',
+      '75': '0.75',
+      '100': '1'
+    },
+    padding: theme => theme('spacing'),
+    stroke: {
+      current: 'currentColor'
+    },
+    textColor: theme => theme('colors'),
+    width: theme => ({
+      auto: 'auto',
+      ...theme('spacing'),
+      '1/2': '50%',
+      '1/3': '33.33333%',
+      '2/3': '66.66667%',
+      '1/4': '25%',
+      '2/4': '50%',
+      '3/4': '75%',
+      full: '100%',
+      screen: '100vw'
+    }),
+    zIndex: {
+      auto: 'auto',
+      '0': '0',
+      '10': '10',
+      '20': '20',
+      '30': '30',
+      '40': '40',
+      '50': '50'
+    }
   },
-  variants: {},
+  variants: {
+    alignContent: [],
+    alignItems: [],
+    alignSelf: [],
+    backgroundColor: ['responsive', 'hover', 'focus'],
+    backgroundPosition: [],
+    backgroundSize: [],
+    borderColor: ['responsive', 'hover', 'focus'],
+    borderRadius: [],
+    borderStyle: [],
+    borderWidth: ['responsive'],
+    boxShadow: ['responsive', 'hover', 'focus'],
+    cursor: ['responsive'],
+    display: ['responsive'],
+    flex: ['responsive'],
+    flexDirection: ['responsive'],
+    flexGrow: ['responsive'],
+    flexShrink: ['responsive'],
+    flexWrap: ['responsive'],
+    float: ['responsive'],
+    fontFamily: [],
+    fontSize: ['responsive'],
+    fontStyle: ['responsive'],
+    fontWeight: ['responsive', 'hover', 'focus'],
+    height: ['responsive'],
+    inset: ['responsive'],
+    justifyContent: ['responsive'],
+    letterSpacing: ['responsive'],
+    lineHeight: [],
+    listStylePosition: ['responsive'],
+    listStyleType: ['responsive'],
+    margin: ['responsive'],
+    maxHeight: ['responsive'],
+    maxWidth: ['responsive'],
+    minHeight: ['responsive'],
+    minWidth: ['responsive'],
+    objectFit: [],
+    objectPosition: [],
+    opacity: ['responsive'],
+    overflow: ['responsive'],
+    padding: ['responsive'],
+    pointerEvents: ['responsive'],
+    position: ['responsive'],
+    textAlign: ['responsive'],
+    textColor: ['responsive', 'hover', 'focus'],
+    textDecoration: ['responsive', 'hover', 'focus'],
+    textTransform: [],
+    verticalAlign: ['responsive'],
+    visibility: ['responsive'],
+    width: ['responsive'],
+    wordBreak: ['responsive'],
+    zIndex: ['responsive']
+  },
+  corePlugins: {
+    appearance: false,
+    backgroundAttachment: false,
+    backgroundRepeat: false,
+    borderCollapse: false,
+    fill: false,
+    fontSmoothing: false,
+    order: false,
+    outline: false,
+    resize: false,
+    stroke: false,
+    userSelect: false,
+    tableLayout: false,
+    whitespace: false
+  },
   plugins: []
-};
+}
