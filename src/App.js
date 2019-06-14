79f69dce5078d5ef25ecd6dc1e8be7a3c05373ce
Amy Townsend <amygtownsend@gmail.com>
2019-06-14 10:05:09 -0400

    use classnames plugin to change styles for every other card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Router, Link } from '@reach/router'
 import BlogExcerpt from './components/BlogExcerpt'
 import json from './data/blog-posts.json'
+const classNames = require('classnames')
 
 const App = () => (
   <div className="max-w-950 m-auto">
@@ -19,15 +20,46 @@ const Home = () => {
     setData(json)
   }, [])
 
-  const renderExcerpt = ({ key, picture, author, title, snippet, date }) => {
+  const renderExcerpt = ({
+    index,
+    key,
+    picture,
+    author,
+    title,
+    snippet,
+    date
+  }) => {
+    const cardClasses = classNames(
+      'group',
+      'flex',
+      'items-start',
+      'p-30',
+      'my-30',
+      'hover:text-white',
+      'rounded-8',
+      {
+        'hover:bg-green-200': index % 2 === 0,
+        'hover:bg-blue-200': !(index % 2 === 0)
+      }
+    )
+
+    const titleClasses = classNames(
+      {
+        'text-green-200': index % 2 === 0,
+        'text-blue-200': !(index % 2 === 0)
+      },
+      'group-hover:text-white',
+      'text-36',
+      'leading-tight',
+      'capitalize'
+    )
+
     return (
       <Link to={key}>
-        <article className="group flex items-start p-30 my-30 hover:bg-green-200 hover:text-white rounded-8">
+        <article className={cardClasses}>
           <img src={picture} alt="" />
           <div className="ml-30">
-            <h2 className="text-green-200 group-hover:text-white text-36 leading-tight capitalize">
-              {title}
-            </h2>
+            <h2 className={titleClasses}>{title}</h2>
             <div className="flex text-18 my-8">
               <h3 className="mr-8">{author}</h3>
               <span className="text-gray-100 group-hover:text-white">|</span>
